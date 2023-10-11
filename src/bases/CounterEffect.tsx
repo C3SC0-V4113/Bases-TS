9530a49595882f7e8d1630805f565785458e3a8b
CESCO <cescovalledev@gmail.com>
2023-10-10 22:10:13 -0600

    using useEffect in typeScript

---
+++ src/bases/CounterEffect.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const MAXIMUM_COUNT = 10;
+
+export const CounterEffect = () => {
+  const [counter, setCounter] = useState(5);
+
+  const handleClick = () => {
+    setCounter((prev) => Math.min(prev + 1, MAXIMUM_COUNT));
+  };
+
+  useEffect(() => {
+    if (counter < 10) return;
+
+    console.log(
+      "%cSe llego al valor maximo",
+      "color: red; background-color:black;"
+    );
+  }, [counter]);
+
+  return (
+    <>
+      <h1>CounterEffect: {counter}</h1>
+      <button onClick={handleClick}>+1</button>
+    </>
+  );
+};
