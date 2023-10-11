9af386866b3f2fb5937637e954038f8a78350615
CESCO <cescovalledev@gmail.com>
2023-10-10 22:22:03 -0600

    implementing gsap

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { gsap } from "gsap";
 
 const MAXIMUM_COUNT = 10;
 
@@ -16,11 +17,16 @@ export const CounterEffect = () => {
       "%cSe llego al valor maximo",
       "color: red; background-color:black;"
     );
+
+    gsap.to("h2", { y: -10, duration: 0.2, ease: "ease.out" }).then(() => {
+      gsap.to("h2", { y: 0, duration: 1, ease: "bounce.out" });
+    });
   }, [counter]);
 
   return (
     <>
-      <h1>CounterEffect: {counter}</h1>
+      <h1>CounterEffect: </h1>
+      <h2>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
