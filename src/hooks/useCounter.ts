2ed054ebf6e8822db1e0f301d35f3ae3936df175
CESCO <cescovalledev@gmail.com>
2023-10-11 23:11:23 -0600

    optimized legibility by making most functionality in a hook

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+const MAXIMUM_COUNT = 10;
+
+export const useCounter = () => {
+  const [counter, setCounter] = useState(5);
+
+  const counterElement = useRef<HTMLHeadingElement>(null);
+
+  const handleClick = () => {
+    setCounter((prev) => Math.min(prev + 1, MAXIMUM_COUNT));
+  };
+
+  useEffect(() => {
+    if (counter < 10) return;
+
+    console.log(
+      "%cSe llego al valor maximo",
+      "color: red; background-color:black;"
+    );
+
+    const timeLine = gsap.timeline();
+
+    timeLine.to(counterElement.current, {
+      y: -10,
+      duration: 0.2,
+      ease: "ease.out",
+    });
+
+    timeLine.to(counterElement.current, {
+      y: 0,
+      duration: 1,
+      ease: "bounce.out",
+    });
+  }, [counter]);
+
+  return {
+    // properties
+    counter,
+    counterElement,
+    //methods
+    handleClick,
+  };
+};
