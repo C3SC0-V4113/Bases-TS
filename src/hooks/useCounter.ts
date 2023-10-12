4e83b0d8701d3c7ed82b6ef3923e45047fb47793
CESCO <cescovalledev@gmail.com>
2023-10-12 00:00:31 -0600

    some optimizations and parametrizations for code

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,44 +1,49 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
-const MAXIMUM_COUNT = 10;
+interface HookProps {
+  maxCount?: number;
+}
 
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 10 }: HookProps) => {
   const [counter, setCounter] = useState(5);
 
-  const counterElement = useRef<HTMLHeadingElement>(null);
+  const elementToAnimate = useRef<HTMLHeadingElement>(null);
+
+  const timeLine = useRef(gsap.timeline());
 
   const handleClick = () => {
-    setCounter((prev) => Math.min(prev + 1, MAXIMUM_COUNT));
+    setCounter((prev) => Math.min(prev + 1, maxCount));
   };
 
+  useLayoutEffect(() => {
+    if (!elementToAnimate.current) return;
+
+    timeLine.current
+      .to(elementToAnimate.current, {
+        y: -10,
+        duration: 0.2,
+        ease: "ease.out",
+      })
+      .to(elementToAnimate.current, {
+        y: 0,
+        duration: 1,
+        ease: "bounce.out",
+      })
+      .pause();
+  }, []);
+
   useEffect(() => {
-    if (counter < 10) return;
+    if (counter < maxCount) return;
 
-    console.log(
-      "%cSe llego al valor maximo",
-      "color: red; background-color:black;"
-    );
-
-    const timeLine = gsap.timeline();
-
-    timeLine.to(counterElement.current, {
-      y: -10,
-      duration: 0.2,
-      ease: "ease.out",
-    });
-
-    timeLine.to(counterElement.current, {
-      y: 0,
-      duration: 1,
-      ease: "bounce.out",
-    });
+    timeLine.current.play(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [counter]);
 
   return {
     // properties
     counter,
-    counterElement,
+    elementToAnimate,
     //methods
     handleClick,
   };
