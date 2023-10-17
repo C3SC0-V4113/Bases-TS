ba0c0ea1c573ed6de7a4c1a4046b94268c12b0d2
CESCO <cescovalledev@gmail.com>
2023-10-17 13:49:31 -0600

    reducer working

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 interface CounterState {
   counter: number;
@@ -7,23 +7,52 @@ interface CounterState {
 }
 
 const INITIAL_STATE: CounterState = {
-  counter: 0,
-  previous: 0,
-  changes: 0,
+  counter: 10,
+  previous: 10,
+  changes: 10,
+};
+
+type CounterAction =
+  | {
+      type: "increaseBy";
+      payload: { value: number };
+    }
+  | {
+      type: "reset";
+    };
+
+const counterReducer = (
+  state: CounterState,
+  action: CounterAction
+): CounterState => {
+  switch (action.type) {
+    case "increaseBy":
+      return state;
+
+    case "reset":
+      return {
+        counter: 0,
+        previous: 0,
+        changes: 0,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export const CounterReducer = () => {
-  const [counter, setCounter] = useState(0);
-
-  const [state, dispatch] = useReducer(first, INITIAL_STATE);
+  const [state, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
   const handleClick = () => {
-    setCounter((prev) => prev + 1);
+    dispatch({
+      type: "reset",
+    });
   };
   return (
     <>
-      <h1>CounterReducer: {counter}</h1>
-      <button onClick={handleClick}>+1</button>
+      <h1>CounterReducer: {state.counter}</h1>
+      <button onClick={handleClick}>reset</button>
     </>
   );
 };
